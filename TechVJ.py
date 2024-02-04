88dff96c6690be22a5fffa35c4d582678645f81b
Harshulsen <150106608+Harshulsen@users.noreply.github.com>
2024-02-04 09:26:50 +0530

    Update TechVJ.py

--- TechVJ.py
+++ TechVJ.py
@@ -2,21 +2,21 @@ import os
 import pyrogram
 from pyrogram import Client
 
-api_id = 19181985
-api_hash = "a2b23ca3a1c9b5dab4bf42dda7de4e79"
+api_id = 27378631
+api_hash = "62b24cf4b91a87847b731a8d652cd859"
 
 try:
    os.remove("techvj.session")
 except:
      pass
 with Client("techvj", api_id=api_id, api_hash=api_hash) as app:
-    session = f"**🥀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 » 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 💞**\n\n`{app.export_session_string()}`\n\n**💥 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲: [VJ Server](https://t.me/VJ_Botz) ✨**"
+    session = f"**🥀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 » 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 💞**\n\n`{app.export_session_string()}`\n\n**💥 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲: [MKV Server](https://t.me/Mkvmovie01) ✨**"
     app.send_message("me", session, disable_web_page_preview=True)
     try:
-        app.join_chat("VJ_Bots")
-        app.join_chat("VJ_Botz")
-        app.join_chat("VJ_Bot_Disscussion")
-        app.join_chat("VJ_Movie")
+        app.join_chat("Mkvmovie01")
+        app.join_chat("mkvbt")
+        app.join_chat("Mkvmovie01")
+        app.join_chat("mkvbt")
     except:
         pass
     print(f"✅ String Session Has 🌟 Been Sent\nTo Your 🔥 Saved Message ✨ ...")
